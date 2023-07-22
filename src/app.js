0a3b4a761de4615d2453942bfd83ae086902b2a1
Anand Kumar <121444538+AnandKumar1008@users.noreply.github.com>
2023-07-22 15:23:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 const app = express();
 
 //Middleware
@@ -7,27 +7,88 @@ app.use(express.json());
 
 const tourDetails = JSON.parse(fs.readFileSync(`${__dirname}/data/tours.json`));
 
-app.get('/tours', (req, res) => {
+app.get("/tours", (req, res) => {
   //write a code here to get all the tours from tours.json
+  try {
+    res.status(200).json({
+      status: 200,
+
+      message: "Success",
+
+      data: [...tourDetails],
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 200,
+
+      message: "Success",
+    });
+  }
 });
 
-app.post('/tours', (req, res) => {
+app.post("/tours", (req, res) => {
   const { name, description, duration, price } = req.body;
+  const data = req.body;
+  data.id = tourDetails.length + 1;
+  tourDetails.push(data);
+  fs.writeFile(
+    `${__dirname}/data/tours.json`,
+    JSON.stringify(tourDetails),
+    (err) => {
+      res.json({
+        status: 200,
+
+        message: "Tour added successfully",
+      });
+    }
+  );
   //Write a code here for creating new tour from data/tours.json
   //For creating new id use this logic
   // const newId = tourDetails[tourDetails.length - 1].id + 1;
 });
 
-app.put('/tours/:id', (req, res) => {
+app.put("/tours/:id", (req, res) => {
   const tourId = parseInt(req.params.id);
   const updatedTour = req.body;
+  updatedTour.id = tourId;
+  let present = false;
+  const item = tourDetails.map((user) => {
+    if (user.id == tourId) {
+      present = true;
+      return { ...user, ...updatedTour };
+    }
+    return user;
+  });
+  if (!present) {
+    return res.status(404).json({ status: 404, message: "Not Found" });
+  }
+  fs.writeFile(`${__dirname}/data/tours.json`, JSON.stringify(item), (err) => {
+    res.status(200).json({
+      status: 200,
+
+      message: "Tour updated successfully",
+    });
+  });
 
   //write a code here for updating a tour
 });
 
-app.delete('/tours/:id', (req, res) => {
+app.delete("/tours/:id", (req, res) => {
   const tourId = parseInt(req.params.id);
   //Write a code here for deleting a tour from data/tours.json
+  const filteredItem = tourDetails.filter((user) => user.id != tourId);
+  if (filteredItem.length == tourDetails.length)
+    return res.status(404).json({ status: 404, message: "Not Found" });
+  fs.writeFile(
+    `${__dirname}/data/tours.json`,
+    JSON.stringify(filteredItem),
+    (err) => {
+      res.json({
+        status: 200,
+        message: "Tour deleted successfully",
+      });
+    }
+  );
 });
 
 module.exports = app;
